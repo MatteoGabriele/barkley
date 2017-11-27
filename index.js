c50384f974498e9645a93cb0406aaf54405c6fa0
Matteo Gabriele <m.gabriele.dev@gmail.com>
2017-11-27 13:45:23 +0100

    style(modules): split methods in different modules

--- index.js
+++ index.js
@@ -1,42 +1,14 @@
-const execa = require('execa')
+const { 
+  exec 
+} = require('./lib/executions')
 
-const getDependencyString = deps => deps.split(',').join(' ')
+const {
+  addDeps,
+  removeDeps
+} = require('./lib/dependencies')
 
 module.exports = {
-  exec (cmds, fallbackCmds) {
-    return execa('yarn', cmds).catch(() => execa('npm', fallbackCmds || cmds))
-  },
-
-  addDeps (deps, dev = false) {
-    const yarnCmds = ['add']
-    const npmCmds = ['-S']
-    const dependencyString = getDependencyString(deps)
-
-    if (dev) {
-      yarnCmds.push('--dev')
-      npmCmds = ['-D']
-    }
-
-    yarnCmds.push(dependencyString)
-    npmCmds.push(dependencyString)
-
-    return module.exports.exec(yarnCmds, npmCmds)
-  },
-
-  removeDepds (deps, dev = false) {
-    const dependencyString = getDependencyString(deps)    
-    const yarnCmds = ['remove']
-    const npmCmds = ['uninstall']
-
-    if (dev) {
-      npmCmds.push('-D')
-    } else {
-      npmCmds.push('-S')
-    }
-
-    yarnCmds.push(dependencyString)    
-    npmCmds.push(dependencyString)
-    
-    return module.exports.exec(yarnCmds, npmCmds)
-  }
+  exec,
+  addDeps,
+  removeDeps
 }
